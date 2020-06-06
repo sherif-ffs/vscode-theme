bfe2393f306dbb05b9bb690ff3e0b3233dc35fa9
Sherif Elmetwally <sge5@njit.edu>
2020-06-05 21:59:03 -0400

    added more colors

--- test.js
+++ test.js
@@ -11,3 +11,13 @@ if (gabe == 'fat') {
 }
 
 console.log('asdasd')
+
+function pearson(x, y){
+    let promedio = (lista) => { return lista.reduce((s, a) => s + a, 0) / lista.length };
+    let n = x.length, prom_x = promedio(x) , prom_y = promedio(y);
+    return (x.map( (e, i, r) => (r = {x:e, y:y[i]}) ).reduce( (s, a) => s + a.x * a.y, 0) - n * prom_x * prom_y) / 
+    ((Math.sqrt(x.reduce( (s, a) => (s + a * a) , 0) - n * prom_x * prom_x)) *
+    (Math.sqrt(y.reduce( (s, a) => (s + a * a) , 0) - n * prom_y * prom_y)));
+}
+
+hmmArray.push(percentRank(human_development_index_array, dataWithValues[i].totalValue[0]))
